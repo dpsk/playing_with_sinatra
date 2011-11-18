a0f8b3d896bc77aeb0bbfadc45e5b6eb5b9e0027
Mikhail Nikalyukin <idups1k@gmail.com>
2011-11-18 18:50:51 +0200

    auth via twitter w/o user creation

--- Gemfile
+++ Gemfile
@@ -3,3 +3,7 @@
  gem 'sinatra'
  gem 'haml'                    
  gem 'activerecord', '~> 3.0'  
+ gem 'omniauth', '0.3.0'
+
+
+
--- basics.rb
+++ basics.rb
@@ -1,6 +1,21 @@
-require 'rubygems'
-require 'sinatra'
+  begin
+    require 'sinatra'
+    require 'omniauth'
+  rescue LoadError
+    require 'rubygems'
+    require 'sinatra'
+    require 'omniauth'
+  end
+  
+  use Rack::Session::Cookie
+  use OmniAuth::Builder do
+    provider :twitter, 'HRFXiTmNfJ1XiVzJ7YGj6g', 'o6bc3kCC1M74s4fMol2qysjOOtA9cAk0XrZfcXvtdwI'
+  end
 
-get '/' do  
-  haml :index
-end 
+  get '/' do
+    haml :index
+  end
+  
+  get '/auth/twitter/callback' do
+    redirect "/"
+  end
